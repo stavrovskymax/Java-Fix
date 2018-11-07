b0ba6da4d1760fdb124e9ae63b25d0a2c999d783
stavrovskymax <stavrovsky96@yandex.ru>
2018-11-07 19:49:34 +0300

    added interface IUsersRepository

--- 4. JspExample/src/main/java/ru/ivmiit/servlets/SignUpServlet.java
+++ 4. JspExample/src/main/java/ru/ivmiit/servlets/SignUpServlet.java
@@ -1,15 +1,30 @@
 package ru.ivmiit.servlets;
 
+import ru.ivmiit.models.User;
+import ru.ivmiit.repositories.IUsersRepository;
+import ru.ivmiit.repositories.UsersRepositoryInMemoryImpl;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
+@WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-                        throws ServletException, IOException {
+    private IUsersRepository iUsersRepository;
 
+    @Override
+    public void init() throws ServletException {
+        this.iUsersRepository = new UsersRepositoryInMemoryImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+        List<User> users = iUsersRepository.findAll();
     }
 }
