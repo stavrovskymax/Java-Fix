c29f876871c07889b9886fdd543751622b587846
stavrovskymax <stavrovsky96@yandex.ru>
2018-11-07 19:34:13 +0300

    UsersRepositoryInMemoryImpl has 3 list users

---
+++ 4. JspExample/src/main/java/ru/ivmiit/repositories/UsersRepositoryInMemoryImpl.java
@@ -0,0 +1,26 @@
+package ru.ivmiit.repositories;
+
+import ru.ivmiit.models.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersRepositoryInMemoryImpl implements IUsersRepository {
+    private List<User> users;
+
+    public UsersRepositoryInMemoryImpl() {
+        this.users = new ArrayList<>();
+        User user = new User("Max", "qwerty", LocalDate.parse("10.05.1996"));
+        User user1 = new User("Max1", "qwerty", LocalDate.parse("10.05.1996"));
+        User user2 = new User("Max2", "qwerty", LocalDate.parse("10.05.1996"));
+
+        users.add(user);
+        users.add(user1);
+        users.add(user2);
+    }
+
+    public List<User> findAll() {
+        return this.users;
+    }
+}
