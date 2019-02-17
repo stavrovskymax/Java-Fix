34cefd4c30063f0ab7c616b844085825cf0ab67c
stavrovskymax <stavrovsky96@yandex.ru>
2019-02-17 21:59:24 +0300

    create User

---
+++ HomeworkJDBC/src/main/java/ru/ivmiit/models/User.java
@@ -0,0 +1,34 @@
+package ru.ivmiit.models;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
