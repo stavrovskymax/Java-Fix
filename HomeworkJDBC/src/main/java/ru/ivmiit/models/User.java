88d66b4402b09bbb406b94743121e3a049fc8970
stavrovskymax <stavrovsky96@yandex.ru>
2019-02-18 19:45:41 +0300

    Added collection Car

--- HomeworkJDBC/src/main/java/ru/ivmiit/models/User.java
+++ HomeworkJDBC/src/main/java/ru/ivmiit/models/User.java
@@ -1,9 +1,12 @@
 package ru.ivmiit.models;
 
+import java.util.List;
+
 public class User {
     private int id;
     private String firstName;
     private String lastName;
+    private List<Car> cars;
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
@@ -15,6 +18,14 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public User(int id, String firstName, String lastName, List<Car> cars) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cars = cars;
+    }
+
 
     public String getFirstName() {
         return firstName;
@@ -30,5 +41,17 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
 }
