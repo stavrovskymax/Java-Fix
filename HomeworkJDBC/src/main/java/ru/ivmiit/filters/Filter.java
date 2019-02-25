ef71246559385196fbf3fe5733c555536cfaee0a
Max <stavrovsky96@yandex.ru>
2019-02-25 18:13:45 +0300

    Create Filter

---
+++ HomeworkJDBC/src/main/java/ru/ivmiit/filters/Filter.java
@@ -0,0 +1,34 @@
+package ru.ivmiit.filters;
+
+import com.sun.deploy.net.HttpRequest;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/products")
+public class Filter implements javax.servlet.Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
+
+        HttpSession session = req.getSession(false);
+
+        if (session == null || session.getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+        } else
+            chain.doFilter(req, resp);
+    }
+
+    public void destroy() {
+
+    }
+}
