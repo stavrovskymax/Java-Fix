6098a004d31357c5d02f4a3dbb95c24e4b190314
Max <stavrovsky96@yandex.ru>
2019-03-04 22:39:36 +0300

    refactoring

--- HomeworkJDBC/src/main/java/ru/ivmiit/dao/UsersDao.java
+++ HomeworkJDBC/src/main/java/ru/ivmiit/dao/UsersDao.java
@@ -5,6 +5,6 @@ import ru.ivmiit.models.User;
 import java.util.List;
 
 public interface UsersDao extends CrudDao<User> {
-    List<User> findAllByFirstName(String fistName);
+    List<User> findAllByFirstName(String firstName);
     boolean exist(String login, String password);
 }
