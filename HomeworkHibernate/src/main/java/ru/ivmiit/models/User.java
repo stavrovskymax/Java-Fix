fd8e2a9475ae6f077b2556a3f75b69c14edf6087
Max <stavrovsky96@yandex.ru>
2019-03-06 00:41:50 +0300

    create

---
+++ HomeworkHibernate/src/main/java/ru/ivmiit/models/User.java
@@ -0,0 +1,73 @@
+package ru.ivmiit.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "hw_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    @OneToMany
+    private List<Car> cars;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String login, String password, List<Car> cars) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.password = password;
+        this.cars = cars;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+}
