7e859ea05f1cfe9ad44a61aa61d9cce7a50a7cf5
Max <stavrovsky96@yandex.ru>
2019-03-06 21:37:54 +0300

    create

---
+++ HomeworkHibernate/src/main/java/ru/ivmiit/models/Car.java
@@ -0,0 +1,47 @@
+package ru.ivmiit.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "hw_car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User userId;
+    private String model;
+
+    public Car() {
+    }
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return model;
+    }
+}
