5f230bfef6c91667bac6fa73a3dd75d54f6baa8a
Max <stavrovsky96@yandex.ru>
2019-03-09 19:09:17 +0300

    implement methods update and delete. update method exist

--- HomeworkHibernate/src/main/java/ru/ivmiit/dao/UsersDaoImpl.java
+++ HomeworkHibernate/src/main/java/ru/ivmiit/dao/UsersDaoImpl.java
@@ -1,5 +1,9 @@
 package ru.ivmiit.dao;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.mindrot.jbcrypt.BCrypt;
 import ru.ivmiit.models.User;
 import ru.ivmiit.utils.HibernateSessionFactoryUtil;
@@ -13,24 +17,45 @@ public class UsersDaoImpl implements UsersDao {
     private final String HQL_SELECT_PASSWORD_BY_LOGIN = "select user.password from User as user where user.login = :param1";
 
     public boolean exist(String login, String password) {
-        List<String> userPasswords = (List<String>) HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery(HQL_SELECT_PASSWORD_BY_LOGIN).setParameter("param1", login).list();
+        // Old implement
+        /*List<String> userPasswords = (List<String>) HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery(HQL_SELECT_PASSWORD_BY_LOGIN).setParameter("param1", login).list();
         if (!userPasswords.isEmpty()) {
             String userPassword = userPasswords.get(0);
+            return BCrypt.checkpw(password, userPassword);
+        }
+        return false;*/
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Criteria userCriteria = session.createCriteria(User.class).add(Restrictions.eq("login", login));
+        User user = (User) userCriteria.uniqueResult();
+        if (user != null) {
+            String userPassword = user.getPassword();
             return BCrypt.checkpw(password, userPassword);
         }
         return false;
     }
 
-    public void save(User model) {
-
+    public void save(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(user);
+        transaction.commit();
+        session.close();
     }
 
-    public void update(User model) {
-
+    public void update(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(user);
+        transaction.commit();
+        session.close();
     }
 
-    public void delete(User model) {
-
+    public void delete(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
     }
 
     @SuppressWarnings("unchecked")
