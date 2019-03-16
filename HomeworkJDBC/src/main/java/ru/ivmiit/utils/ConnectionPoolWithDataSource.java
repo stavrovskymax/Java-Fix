fa1e2f8c570f3c81172595e0d2587cde511f9f7c
Max <stavrovsky96@yandex.ru>
2019-03-16 14:35:24 +0300

    create

---
+++ HomeworkJDBC/src/main/java/ru/ivmiit/utils/ConnectionPoolWithDataSource.java
@@ -0,0 +1,42 @@
+package ru.ivmiit.utils;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConnectionPoolWithDataSource {
+    private static ConnectionPoolWithDataSource connectionPool;
+    private static Properties properties = new Properties();
+    private static DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+    static {
+        connectionPool = new ConnectionPoolWithDataSource();
+        try {
+            properties.load(new FileInputStream(ConnectionPoolWithDataSource.class.getClassLoader().getResource("db.properties").getFile()));
+            String dbUrl = properties.getProperty("db.url");
+            String dbUsername = properties.getProperty("db.username");
+            String dbPassword = properties.getProperty("db.password");
+            String driverClassName = properties.getProperty("db.driverClassName");
+
+            dataSource.setUrl(dbUrl);
+            dataSource.setUsername(dbUsername);
+            dataSource.setPassword(dbPassword);
+            dataSource.setDriverClassName(driverClassName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static ConnectionPoolWithDataSource getConnectionPool() {
+        return connectionPool;
+    }
+
+    public DriverManagerDataSource getDataSource() {
+        return dataSource;
+    }
+
+
+}
