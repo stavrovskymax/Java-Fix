7babd714e99053c696855e5f75792e00214a9b75
Max <stavrovsky96@yandex.ru>
2019-03-17 17:54:35 +0300

    added constructor

--- HomeworkHibernate/src/main/java/ru/ivmiit/models/User.java
+++ HomeworkHibernate/src/main/java/ru/ivmiit/models/User.java
@@ -36,6 +36,14 @@ public class User {
         this.cars = cars;
     }
 
+    public User(int id, String firstName, String lastName, String login, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.password = password;
+    }
+
     public int getId() {
         return id;
     }
