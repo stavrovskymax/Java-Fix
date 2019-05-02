62f649975c944e2056d11603b0218ffc7801c347
Max <stavrovsky96@yandex.ru>
2019-05-02 21:20:58 +0300

    added matchers /users, /signup, /profile and form login

---
+++ HomeworkSpringBoot/src/main/java/ru/homework/security/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package ru.homework.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/users/**").authenticated()
+                    .antMatchers("/signup/**").permitAll()
+                    .antMatchers("/profile").permitAll()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("login")
+                    .defaultSuccessUrl("/");
+        http.csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+}
