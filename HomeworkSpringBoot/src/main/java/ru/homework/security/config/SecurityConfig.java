af4ad660bd23069d5ed1f4ca3b8900d86749716d
Max <stavrovsky96@yandex.ru>
2019-05-04 18:17:16 +0300

    added rememberMe and antMatchers(/ and /profile)

--- HomeworkSpringBoot/src/main/java/ru/homework/security/config/SecurityConfig.java
+++ HomeworkSpringBoot/src/main/java/ru/homework/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.homework.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -19,23 +24,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/users/**").authenticated()
+                    .antMatchers("/users/**").hasAnyAuthority("ADMIN")
                     .antMatchers("/signup/**").permitAll()
-                    .antMatchers("/profile").permitAll()
+                    .antMatchers("/profile/**").permitAll()
+                    .antMatchers("/**").permitAll()
                     .and()
                 .formLogin()
                     .loginPage("/login")
                     .usernameParameter("login")
-                    .defaultSuccessUrl("/");
+                    .defaultSuccessUrl("/profile")
+                    .and()
+                .rememberMe()
+                    .rememberMeParameter("remember-me")
+                    .tokenRepository(tokenRepository());
         http.csrf().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
     }
 }
