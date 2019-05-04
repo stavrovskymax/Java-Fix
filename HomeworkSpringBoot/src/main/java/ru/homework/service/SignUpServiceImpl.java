890cd973bbfc2cf60f9f2d1286f12e271c6b0bd5
Max <stavrovsky96@yandex.ru>
2019-05-04 18:19:55 +0300

    fixed bag in User builder and implemented authenticateUserAndSetSession() method

--- HomeworkSpringBoot/src/main/java/ru/homework/service/SignUpServiceImpl.java
+++ HomeworkSpringBoot/src/main/java/ru/homework/service/SignUpServiceImpl.java
@@ -1,7 +1,12 @@
 package ru.homework.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Service;
 import ru.homework.forms.UserForm;
 import ru.homework.models.Car;
@@ -10,6 +15,7 @@ import ru.homework.models.State;
 import ru.homework.models.User;
 import ru.homework.repositories.UsersRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 @Service
@@ -21,6 +27,9 @@ public class SignUpServiceImpl implements SignUpService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
     public void signUp(UserForm userForm) {
         String passwordHash = passwordEncoder.encode(userForm.getPassword());
@@ -28,7 +37,7 @@ public class SignUpServiceImpl implements SignUpService {
         User user = User.builder()
                 .firstName(userForm.getFirstName())
                 .lastName(userForm.getLastName())
-                .login(userForm.getLastName())
+                .login(userForm.getLogin())
                 .passwordHash(passwordHash)
                 .cars(new ArrayList<Car>())
                 .state(State.ACTIVE)
@@ -42,5 +51,19 @@ public class SignUpServiceImpl implements SignUpService {
 
         user.getCars().add(car);
         usersRepository.save(user);
+    }
+
+    public void authenticateUserAndSetSession(UserForm user, HttpServletRequest request) {
+        String login = user.getLogin();
+        String password = user.getPassword();
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(login, password);
+
+        // generate session if one doesn't exist
+        request.getSession();
+
+        token.setDetails(new WebAuthenticationDetails(request));
+        Authentication authenticatedUser = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
     }
 }
