efc48e6a98287985d6a85911747bc499697d3f8f
Max <stavrovsky96@yandex.ru>
2019-05-04 18:14:46 +0300

    added order by to query of findUsersWithCars method

--- HomeworkSpringBoot/src/main/java/ru/homework/repositories/UsersRepository.java
+++ HomeworkSpringBoot/src/main/java/ru/homework/repositories/UsersRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<User, Long> {
     @Query("select new ru.homework.transfer.UserDto(user.firstName, user.lastName, car.model) " +
-            "from User user left join user.cars as car")
+            "from User user left join user.cars as car order by user.id")
     List<User> findUsersWithCars();
 
     Optional<User> findUserByLogin(String login);
