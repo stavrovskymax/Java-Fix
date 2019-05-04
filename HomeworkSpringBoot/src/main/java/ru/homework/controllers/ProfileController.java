7d26ff9a4cf245c9557952e1a9d3e5759a15bdac
Max <stavrovsky96@yandex.ru>
2019-05-05 02:18:59 +0300

    added addCar() method

--- HomeworkSpringBoot/src/main/java/ru/homework/controllers/ProfileController.java
+++ HomeworkSpringBoot/src/main/java/ru/homework/controllers/ProfileController.java
@@ -5,9 +5,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.homework.forms.CarsForm;
 import ru.homework.models.Car;
 import ru.homework.repositories.CarsRepository;
 import ru.homework.security.details.UserDetailsImpl;
+import ru.homework.service.ProfileService;
 import ru.homework.transfer.UserDto;
 
 import java.util.List;
@@ -18,16 +21,27 @@ public class ProfileController {
     @Autowired
     private CarsRepository carsRepository;
 
+    @Autowired
+    private ProfileService service;
+
     @GetMapping("/profile")
     public String getProfilePage(Authentication authentication, ModelMap model) {
         if (authentication == null) {
             return "redirect:/login";
         }
+
         UserDetailsImpl details = (UserDetailsImpl) authentication.getPrincipal();
         UserDto user = UserDto.form(details.getUser());
+
         List<Car> cars = carsRepository.findCarsByOwner(details.getUser());
         model.addAttribute("user", user);
         model.addAttribute("cars", cars);
         return "profile";
+    }
+
+    @PostMapping("/profile")
+    public String addCar(CarsForm carsForm, Authentication authentication) {
+        service.addCar(carsForm, authentication);
+        return "redirect:/profile";
     }
 }
