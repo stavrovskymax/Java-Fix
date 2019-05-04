2f5597be4e407095050466189418536ab0e46347
Max <stavrovsky96@yandex.ru>
2019-05-04 17:38:47 +0300

    created

---
+++ HomeworkSpringBoot/src/main/java/ru/homework/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package ru.homework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "redirect:/profile";
+    }
+}
