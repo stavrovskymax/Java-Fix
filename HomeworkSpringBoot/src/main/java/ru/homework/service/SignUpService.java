bd0bf1fa8ba877cf172e35715b59447aeb4a09a7
Max <stavrovsky96@yandex.ru>
2019-05-04 18:17:55 +0300

    added authenticateUserAndSetSession() method

--- HomeworkSpringBoot/src/main/java/ru/homework/service/SignUpService.java
+++ HomeworkSpringBoot/src/main/java/ru/homework/service/SignUpService.java
@@ -2,6 +2,9 @@ package ru.homework.service;
 
 import ru.homework.forms.UserForm;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface SignUpService {
     void signUp(UserForm userForm);
+    void authenticateUserAndSetSession(UserForm user, HttpServletRequest request);
 }
